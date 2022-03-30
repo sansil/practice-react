ddc145751badee4019dc7b8614781e76db7f5014
sansil <sansil32@gmail.com>
2022-03-30 16:17:32 -0300

    advance

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { createStore } from 'redux'
+
+let store = createStore()
 
 ReactDOM.render(
   <React.StrictMode>
